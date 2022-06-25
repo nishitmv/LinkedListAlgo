4a74475c2c6a2872b6a382429ecfb117520d4484
nishit <nishitmv@gmail.com>
2022-06-26 01:38:36 +0530

    Remove Nth Node from end of LinkedList

---
+++ src/main/java/com/nitron/linkedlists/RemoveNthNodeFromEnd/LinkedListNthNodeRemover.java
@@ -0,0 +1,39 @@
+package com.nitron.linkedlists.RemoveNthNodeFromEnd;
+
+import com.nitron.linkedlists.ListNode;
+
+public class LinkedListNthNodeRemover {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode pointer = head;
+
+        int length =0;
+        while( pointer !=null)
+        {
+            pointer = pointer.next;
+            length++;
+        }
+
+        if(n>length)
+            return null;
+
+        if(n==length)
+        {
+            head = head.next;
+            return head;
+        }
+
+        pointer = head;
+        for(int i=1;i<length-n;i++)
+        {
+            pointer= pointer.next;
+        }
+
+
+        ListNode temp = pointer.next;
+        pointer.next = pointer.next.next;
+        temp.next= null;
+        return head;
+    }
+}
