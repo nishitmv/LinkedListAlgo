3e8c7f0bce0a7fcf0463ecac174ba8fa4d6b8d92
nishit <nishitmv@gmail.com>
2022-06-26 01:18:24 +0530

    Find Intersection Node between two linkedLists

---
+++ src/main/java/com/nitron/linkedlists/FindIntersectionNodeBetweenTwoLists/LinkedListIntercestionNodeFinder.java
@@ -0,0 +1,57 @@
+package com.nitron.linkedlists.FindIntersectionNodeBetweenTwoLists;
+
+public class LinkedListIntercestionNodeFinder {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode pointer1 = headA;
+        ListNode pointer2 = headB;
+
+
+        // Get Lengths of both arrays
+        int length1 = 0;
+        while(pointer1!=null)
+        {
+            length1++;
+            pointer1 = pointer1.next;
+        }
+        int length2 = 0;
+        while(pointer2!=null)
+        {
+            length2++;
+            pointer2 = pointer2.next;
+        }
+
+        pointer1 = headA;
+        pointer2 = headB;
+
+        // Move longer list head pointer to level with smaller list length
+
+        if(length1<length2)
+        {
+
+            for(int i =0;i<length2-length1;i++)
+                pointer2 = pointer2.next;
+        }
+        else if(length1>length2)
+        {
+            for(int i =0;i<length1-length2; i++)
+                pointer1 = pointer1.next;
+        }
+
+        // Now that both lists are of same lengths , move one step at a time and compare :)
+        while(pointer1!=null && pointer2!=null)
+        {
+            if(pointer1==pointer2)
+                return pointer1;
+
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+
+        }
+
+        return null;
+
+
+    }
+}
---
+++ src/main/java/com/nitron/linkedlists/FindIntersectionNodeBetweenTwoLists/ListNode.java
@@ -0,0 +1,10 @@
+package com.nitron.linkedlists.FindIntersectionNodeBetweenTwoLists;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
